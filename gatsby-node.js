742e88122c39b02c4bd9938c30d4f8d95ccb7d4b
James Cowan <james.cowan@ehealth.com>
2022-05-18 09:11:40 -0500

    Add yoast xsl, add pathPrefix

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,64 +7,88 @@ const { extractUrls } = require("./parse-sitemap");
 const withoutTrailingSlash = (path) => path === `/` ? path : path.replace(/\/$/, ``);
 
 if (!String.prototype.replaceAll) {
-  String.prototype.replaceAll = function (find, replace) {
-    return this.split(find).join(replace);
-  };
+    String.prototype.replaceAll = function (find, replace) {
+        return this.split(find).join(replace);
+    };
 }
 
 exports.createPages = async ({ actions, graphql }, options) => {
-  const { createRedirect } = actions;
+    const { createRedirect } = actions;
 
-  let { baseUrl, gatsbyUrl, auth, publicPath } = options || {};
+    let { baseUrl, gatsbyUrl, auth, publicPath, pathPrefix = '' } = options || {};
 
-  if (!baseUrl) {
-    throw new Error("gatsby-plugin-yoast-sitemap: You must define the url for your Wordpress Install. This should not contain /graphql, if using gatsby-source-wordpress.")
-  }
-
-  if (!gatsbyUrl) {
-    throw new Error("gatsby-plugin-yoast-sitemap: You must define the URL for your Gatsby-hosted site. This may be localhost.")
-  }
-
-  if (!publicPath) publicPath = `./public`
-
-  gatsbyUrl = gatsbyUrl.replace("https://", "");
-  gatsbyUrl = gatsbyUrl.replace("http://", "");
-
-  let siteMapIndex = await axios.get(
-    `https://${withoutTrailingSlash(baseUrl)}/sitemap_index.xml`,
-    {
-      auth: auth
+    if (!baseUrl) {
+        throw new Error("gatsby-plugin-yoast-sitemap: You must define the url for your Wordpress Install. This should not contain /graphql, if using gatsby-source-wordpress.")
     }
-  );
-  const downloadableXMLNodes = await extractUrls(siteMapIndex.data);
-  siteMapIndex = siteMapIndex.data.replaceAll(baseUrl, gatsbyUrl);
-  siteMapIndex = siteMapIndex.replaceAll(
-    `http://${gatsbyUrl}`,
-    `https://${gatsbyUrl}`
-  );
-  await fse.outputFile(
-    path.join(publicPath, `sitemap_index.xml`),
-    siteMapIndex
-  );
 
-  for (const node of downloadableXMLNodes) {
-    let mapFromNode = await axios.get(node, {
-      auth: auth,
-    });
-    mapFromNode = mapFromNode.data.replaceAll(baseUrl, gatsbyUrl);
-    mapFromNode = mapFromNode.replaceAll(`https://${gatsbyUrl}`, `https://${gatsbyUrl}`);
-    const url_parts = url.parse(node);
-    await fse.outputFile(
-      path.join(publicPath, url_parts.pathname),
-      mapFromNode
+    if (!gatsbyUrl) {
+        throw new Error("gatsby-plugin-yoast-sitemap: You must define the URL for your Gatsby-hosted site. This may be localhost.")
+    }
+
+    if (!publicPath) publicPath = `./public`
+
+    const gatsbyDomain = gatsbyUrl
+        .replace("https://", "")
+        .replace("http://", "");
+
+    const baseDomain = baseUrl
+        .replace("https://", "")
+        .replace("http://", "");
+
+    let siteMapIndex = await axios.get(
+        `${withoutTrailingSlash(baseUrl)}/sitemap_index.xml`,
+        {
+            auth: auth
+        }
     );
-  }
+    let styleSheet = await axios.get(
+        `${withoutTrailingSlash(baseUrl)}/wp-content/plugins/wordpress-seo/css/main-sitemap.xsl`,
+        {
+            auth: auth
+        }
+    );
+    styleSheet = styleSheet.data;
+    const downloadableXMLNodes = await extractUrls(siteMapIndex.data);
+    siteMapIndex = siteMapIndex.data.replaceAll(baseDomain, `${gatsbyDomain}${pathPrefix}`);
 
-  createRedirect({
-    fromPath: `/sitemap.xml`,
-    toPath: `/sitemap_index.xml`,
-    isPermanent: false,
-    redirectInBrowser: true,
-    force: true,
-  });
+    siteMapIndex = siteMapIndex.replace(
+        `wp-content/plugins/wordpress-seo/css/main-sitemap.xsl`,
+        `/stylesheet.xsl`
+    );
+
+    await fse.outputFile(
+        path.join(publicPath, `sitemap_index.xml`),
+        siteMapIndex
+    );
+    await fse.outputFile(
+        path.join(publicPath, `stylesheet.xsl`),
+        styleSheet
+    );
+
+    for (const node of downloadableXMLNodes) {
+        let mapFromNode = await axios.get(node, {
+            auth: auth,
+        });
+        mapFromNode = mapFromNode.data.replaceAll(baseDomain, `${gatsbyDomain}${pathPrefix}`);
+
+        mapFromNode = mapFromNode.replace(
+            `wp-content/plugins/wordpress-seo/css/main-sitemap.xsl`,
+            `/stylesheet.xsl`
+        );
+
+        const url_parts = url.parse(node);
+
+        await fse.outputFile(
+            path.join(publicPath, url_parts.pathname),
+            mapFromNode
+        );
+    }
+
+    createRedirect({
+        fromPath: `/sitemap.xml`,
+        toPath: `/sitemap_index.xml`,
+        isPermanent: false,
+        redirectInBrowser: true,
+        force: true,
+    });
 };
